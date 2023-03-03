7ef08fa22cc8fe23b618276bfbb25284a92b7ee6
ryanpearl48 <ryanpearl98@gmail.com>
2023-03-03 13:58:38 -0800

    Functional admin privileges 2

--- client/app/store.js
+++ client/app/store.js
@@ -1,25 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
-// import authReducer from '../features/auth/authSlice';
-// import editUserSlice from '../features/editUser/editUserSlice';
-// import cartReducer from '../features/cart/cartSlice';
-// import singleUserSlice from '..features/userPage/userPageSlice';
-// import allProductsReducer from '../features/products/AllProductsSlice';
-
-// const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     editUser: editUserSlice,
-//     cart: cartReducer,
-//     singleUser: singleUserSlice,
-//     allProducts: allProductsReducer 
-//   },
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-// });
-
-// export default store;
-// export * from '../features/auth/authSlice';
-
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
@@ -27,14 +5,16 @@ import editUserSlice from '../features/editUser/editUserSlice';
 import cartReducer from '../features/cart/cartSlice';
 import singleUserSlice from '../features/userPage/userPageSlice';
 import allProductsReducer from '../features/products/AllProductsSlice';
+import allUsersReducer from '../features/admin/adminSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     editUser: editUserSlice,
     singleUser: singleUserSlice,
-    cart: cartReducer, 
-    allProducts: allProductsReducer
+    cart: cartReducer,
+    allProducts: allProductsReducer,
+    allUsers: allUsersReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
