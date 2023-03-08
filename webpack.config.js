9384a7802f20de6fd65a40d45632a09a6b150151
Cody DiBella <30646495+CodyDiBella@users.noreply.github.com>
2023-03-08 12:23:20 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,40 @@ const path = require('path');
 module.exports = {
   entry: ['./client/index.js'],
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
-  context: __dirname,
+  mode: 'production',
   devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
