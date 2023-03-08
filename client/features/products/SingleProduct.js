f083b1db7a1eb0da3e6d5abab62d639fb20da4f8
rany3l <mr.ronnie.angeles@gmail.com>
2023-03-07 19:42:08 -0600

    css style

--- client/features/products/SingleProduct.js
+++ client/features/products/SingleProduct.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchSingleProduct, selectSingleProduct } from './SingleProductSlice';
-import { useParams } from 'react-router-dom';
-import { addOrder } from '../dataSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSingleProduct, selectSingleProduct } from "./SingleProductSlice";
+import { useParams } from "react-router-dom";
+import { addOrder } from "../dataSlice";
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
@@ -15,14 +15,14 @@ const SingleProduct = () => {
   const handleAddToCart = (id, name, price, userId) => {
     if (isLoggedIn) {
       const product = { id, name, price };
-      fetch('/api/orders', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      fetch("/api/orders", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           userId,
           product: JSON.stringify(product),
-          quantity: 1
-        })
+          quantity: 1,
+        }),
       })
         .then((response) => response.json())
         .then((data) => {
@@ -31,8 +31,11 @@ const SingleProduct = () => {
         .catch((error) => console.error(error));
     } else {
       const order = { productName: name, productPrice: price, quantity: 1 };
-      const existingOrders = JSON.parse(localStorage.getItem('orders')) || [];
-      localStorage.setItem('orders', JSON.stringify([...existingOrders, order]));
+      const existingOrders = JSON.parse(localStorage.getItem("orders")) || [];
+      localStorage.setItem(
+        "orders",
+        JSON.stringify([...existingOrders, order])
+      );
     }
   };
 
@@ -41,7 +44,7 @@ const SingleProduct = () => {
   }, [dispatch, id]);
 
   return (
-    <div>
+    <div className="singleP">
       {product && (
         <>
           <h3>{product.name}</h3>
@@ -49,14 +52,18 @@ const SingleProduct = () => {
           <p>{product.description}</p>
           <p>${product.price}</p>
           {isLoggedIn && (
-            <button onClick={() => handleAddToCart(product.id, product.name, product.price, userId)}>
+            <button
+              onClick={() =>
+                handleAddToCart(product.id, product.name, product.price, userId)
+              }
+            >
               Add to Cart
             </button>
-          )}        </>
+          )}{" "}
+        </>
       )}
     </div>
   );
-}
+};
 
 export default SingleProduct;
-
